5bdeb87c05dc5bd9c8d0f8123b4d45da6ae87988
vishnu <vishnu452@gmail.com>
2026-02-21 14:48:04 +0530

    testing hook

--- spec/app/hooks/useAgentOverview/useAgentOverView.test.ts
+++ spec/app/hooks/useAgentOverview/useAgentOverView.test.ts
@@ -6,7 +6,6 @@ import useAgentOverview from '@src/app/hooks/useAgentOverview';
 
 describe(`${useAgentOverview.name}: client`, () => {
   const props = factories.agentOverviewProps.build();
-
   it('should return the correct data', () => {
     const { result } = renderHook(() => useAgentOverview(props));
     expect(result.current).toEqual({});
